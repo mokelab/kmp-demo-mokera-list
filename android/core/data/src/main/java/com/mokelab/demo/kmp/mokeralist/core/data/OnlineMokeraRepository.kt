260e6b0fa007c7b887616b7078e6bf062f6e50f0
Hirokazu Fukami <fkm@mokelab.com>
2025-06-30 10:50:45 +0900

    Add OnlineMokeraRepository implementation for fetching Mokeras

---
+++ android/core/data/src/main/java/com/mokelab/demo/kmp/mokeralist/core/data/OnlineMokeraRepository.kt
@@ -0,0 +1,17 @@
+package com.mokelab.demo.kmp.mokeralist.core.data
+
+import com.mokelab.demo.kmp.mokeralist.core.model.Mokera
+import com.mokelab.demo.kmp.mokeralist.core.network.NetworkMokeraDataSource
+
+/**
+ * Repository implementation that fetches Mokeras from a remote server.
+ *
+ * @property dataSource The network data source used to fetch Mokeras.
+ */
+class OnlineMokeraRepository(
+    private val dataSource: NetworkMokeraDataSource,
+) : MokeraRepository {
+    override suspend fun getAll(): List<Mokera> {
+        return dataSource.fetchMokeras()
+    }
+}
